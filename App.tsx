5111995cf1d41cdb852a311faea3eb5240f2b3a4
thereallucas98 <lucas1998david@gmail.com>
2024-07-03 07:19:05 -0300

    chore: babel & metro config; styled-components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,44 @@
+/* eslint-disable camelcase */
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Loading } from '~/components/loading'
 import { SignIn } from '~/screens/sign-in'
+import theme from '~/theme'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  return <SignIn />
+  const [loaded, error] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [error, loaded])
+
+  if (!loaded) {
+    return <Loading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
+  )
 }
